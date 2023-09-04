6d717ec6e85e6d003e6b9281c5bb68c29aff491d
Vignesh Aravindh B <118040393+codechiefVignesh@users.noreply.github.com>
2023-09-04 14:50:23 +0530

    Add files via upload

---
+++ DAA/buick.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include"quick.h"
+using namespace std;
+int main()
+{
+	int n = 1+(rand()%5000);
+	int ptr[n];
+	for(int i = 0; i<n; i++)
+	{
+		ptr[i] = 1+rand()%5000;
+	}
+	quickSort(ptr,0,n-1);
+	for(int k = 0; k<n; k++)
+	{
+		cout<<ptr[k]<<endl;
+	}
+}
+
---
+++ DAA/quick.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+void bubbleSort(int arr[],int low, int high)
+{
+	int size = high-low+1;
+	for(int i = 0; i<size; i++)
+	{
+		for(int j = 0; j<size; j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+	for (int j = low; j <= high - 1; j++) {
+		if (arr[j] < pivot) 
+		{
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+void quickSort(int arr[], int low, int high)
+{
+	if(high-low+1>30)
+	{
+    	if (low < high)
+    	{
+        	int pi = partition(arr, low, high);
+        	quickSort(arr, low, pi - 1);
+        	quickSort(arr, pi + 1, high);
+    	}
+    }
+    else
+    
+    {
+    bubbleSort(arr,low,high);
+    }
+}
+

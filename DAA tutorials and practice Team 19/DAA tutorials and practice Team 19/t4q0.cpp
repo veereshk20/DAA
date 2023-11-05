8a7e0163fd0ca0b6642c5b67a77e9013e0bb8515
Ashrithiiitdm <141637469+Ashrithiiitdm@users.noreply.github.com>
2023-11-05 22:31:34 +0530

    Update and rename t4q1.cpp to t4q0.cpp

---
+++ DAA tutorials and practice Team 19/DAA tutorials and practice Team 19/t4q0.cpp
@@ -0,0 +1,57 @@
+#include<bits\stdc++.h>
+using namespace std;
+void check_reflexive(vector<vector<int>>&v,int m){
+    for(int i=1;i<=m;i++)
+    {
+        if(v[i][i]!=1)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    cout<<"Not reflexive";
+    else
+    cout<<"Reflexive";
+}
+
+void check_symmetric(vector<vector<int>>&v,int m,int n){
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(v[i][j]==1 && v[j][i]!=1)
+            {
+                flag=1;
+                break;
+            }
+        }
+    }
+    if(flag==1)
+    cout<<"Not symmetic"<<endl;
+    else
+    cout<<"Symmetric"<<endl;
+}
+int main()
+{
+    int n,m,flag=0;
+    cin>>n>>m;
+    vector<vector<int>>v(n+1,vector<int>(n+1,0));
+    for(int i=1;i<=m;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        v[x][y]=1;
+        // v[y][x]=1 ;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    check_reflexive(v,m);
+    check_symmetric(v,m,n);
+}
